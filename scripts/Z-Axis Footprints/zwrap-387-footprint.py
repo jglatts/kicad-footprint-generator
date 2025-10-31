7f2035ba24d8c0aa3ab55a8df3a8c02750ad3bbc
John Glatts <jglatts@z-axiscc.com>
2025-10-31 10:51:14 -0400

    add working zacc footprint script

---
+++ scripts/Z-Axis Footprints/zwrap-387-footprint.py
@@ -0,0 +1,75 @@
+"""
+Zwrap-387 Footprint Generator for KiCad
+
+This script uses the KicadModTree Python API to programmatically create a custom 
+KiCad footprint for the Zwrap-387 pad configuration. 
+
+Features:
+- Generates 216 SMT pads arranged in two rows per column.
+- Pads are rectangular with a specific pitch and dimensions in inches (converted from mm).
+- Sets footprint metadata such as description and tags.
+- Outputs a `.kicad_mod` file ready for use in KiCad PCB layouts.
+- Prints debug information about the footprint structure for verification.
+
+Usage:
+    python zwrap387_footprint.py
+
+Author: John Glatts
+Date: 2025-10-31
+"""
+import sys
+import os
+
+sys.path.append(os.path.join(sys.path[0],".."))
+sys.path.append('../..') # enable package import from parent directory
+
+from KicadModTree import *
+from KicadModTree.nodes.specialized.PadArray import PadArray
+
+class Zwrap387Footprint():
+    def __init__(self):
+        self.footprint_name = "zwrap-387-test-pads"
+        self.kicad_mod = Footprint(self.footprint_name)
+
+    def setFootprint(self):
+        if (self.kicad_mod != None):
+            self.kicad_mod.setDescription("footprint for wrap-387 pads")
+            self.kicad_mod.setTags("zacc footprint")            
+
+    def createPads(self):
+        padNumber = 1
+        padX = 0
+        pitchX = .0045 * 25.4
+        numPads = 216
+
+        for i in range(numPads):
+            # make the coppers pad
+            self.kicad_mod.append(Pad(number=padNumber, type=Pad.TYPE_SMT, 
+                                      shape=Pad.SHAPE_RECT, at=[padX,0], 
+                                      size=[0.002*25.4,0.234*25.4], layers=['F.Cu']))
+            
+            self.kicad_mod.append(Pad(number=padNumber+1, type=Pad.TYPE_SMT, 
+                                      shape=Pad.SHAPE_RECT, at=[padX,0.33*25.4], 
+                                      size=[0.002*25.4,0.234*25.4], layers=['F.Cu']))
+            padNumber += 2
+            padX += pitchX
+
+    def printFootprintInfo(self): 
+        print(self.kicad_mod)
+        print(self.kicad_mod.getRenderTree())
+        print(self.kicad_mod.getCompleteRenderTree())
+
+    def save(self, name):
+        file_handler = KicadFileHandler(self.kicad_mod)
+        file_handler.writeFile(name)
+    
+    def makeFootprint(self):
+        self.setFootprint()
+        self.createPads()
+        self.printFootprintInfo()
+        self.save(self.footprint_name + ".kicad_mod")        
+
+
+
+if __name__ == '__main__':
+    Zwrap387Footprint().makeFootprint()
